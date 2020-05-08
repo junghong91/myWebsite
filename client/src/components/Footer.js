9cc802a191cbfa5dcfc6c6b7a88d4de49eca0bd8
Jung <junghong91@naver.com>
2020-05-08 17:52:56 +0900

    link to github, facebook, instagram is added to the navbar

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,21 +4,22 @@ import { BottomNavigation, BottomNavigationAction } from "@material-ui/core";
 import Facebook from "@material-ui/icons/Facebook";
 import Instagram from "@material-ui/icons/Instagram";
 import GitHub from "@material-ui/icons/GitHub";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles({
   root: {
     "& .MuiBottomNavigationAction-root": {
       minWidth: "0",
-      maxWidth: "250px"
+      maxWidth: "250px",
     },
     "& .MuiSvgIcon-root": {
       fill: "tan",
       "&:hover": {
         fill: "tomato",
-        fontSize: "1.8rem"
-      }
-    }
-  }
+        fontSize: "1.8rem",
+      },
+    },
+  },
 });
 
 const Footer = () => {
@@ -27,16 +28,22 @@ const Footer = () => {
     <div>
       <BottomNavigation width="auto" style={{ background: "#222" }}>
         <BottomNavigationAction
+          href="https://www.facebook.com/people/%EC%A0%95%ED%99%8D/100001442749014"
+          target="_blank"
           className={classes.root}
           style={{ padding: 0 }}
           icon={<Facebook />}
         ></BottomNavigationAction>
         <BottomNavigationAction
+          href="https://www.instagram.com/__hongy/"
+          target="_blank"
           className={classes.root}
           style={{ padding: 0 }}
           icon={<Instagram />}
         ></BottomNavigationAction>
         <BottomNavigationAction
+          href="https://github.com/junghong91"
+          target="_blank"
           className={classes.root}
           style={{ padding: 0 }}
           icon={<GitHub />}
